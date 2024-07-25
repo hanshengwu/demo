d4f1ef71c3539b583f8a168f6d3e576d104491f7
hanshengwu <57554061+hanshengwu@users.noreply.github.com>
2024-07-25 18:43:16 +0800

    Update index.js

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,38 +1,24 @@
 module.exports.handler = async (event, context) => {
   const express = require('express');
-  const mysql = require('mysql');
-  const app = express();
-  const db = mysql.createConnection({
-    host: '39.106.39.142',
-    user: 'warrior',
-    password: 'test',
-    database: 'data',
-  });
-  db.connect((err) => {
-    if (err) {
-      console.error('Failed to connect to the database:', err);
-      return;
-    }
-    console.log('Connected to MySQL database');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+// 创建代理中间件
+const apiProxy = createProxyMiddleware('/queryDoc', {
+  target: 'https://cfws.samr.gov.cn',
+  changeOrigin: true, // 设置为 true，以便正确修改请求头中的 Host
+  secure: false, // 忽略证书中的主机名检查（仅用于测试，不建议在生产环境中使用）
 });
 
-app.use(express.json());
+// 将代理中间件挂载到 Express 应用上
+app.use(apiProxy);
 
-// 获取所有数据
-app.get('/api/persons', (req, res) => {
-  const sql = 'SELECT * FROM data.per_capita_table';
-  db.query(sql, (err, results) => {
-    if (err) {
-      res.status(500).json({ error: 'Database error' });
-      return;
-    }
-    res.json(results);
-  });
-});
-
+const port = 8091;
 app.listen(port, () => {
-  console.log(`API服务器已启动在端口${port}`);
+  console.log(`Proxy server listening on port ${port}`);
 });
+
 
 }
 
