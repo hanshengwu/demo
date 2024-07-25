70f76afeeac24a8ab4e5a83de4811ba197b8b581
hanshengwu <57554061+hanshengwu@users.noreply.github.com>
2024-07-25 19:20:31 +0800

    Delete index.js

--- API/test/index.js
+++
@@ -1,9 +0,0 @@
-module.exports.handler = async (event, context) => {
-  const { name } = event.queryStringParameters
-  const message = `Hello ${name || "world"}!`
-
-  return {
-    statusCode: 200,
-    body: message
-  }
-}
